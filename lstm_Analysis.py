5304f01df8008bcb6edde4d910f7df650343ea4c
ParkerQH <blueskyaoa7@gmail.com>
2025-07-16 15:10:08 +0900

    이미지 로드 일부 수정

--- lstm_Analysis.py
+++ lstm_Analysis.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -8,9 +9,8 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(static_image_mode=True)
 
 # 관절 데이터 추출
-def extract_keypoints_from_image(image_path):
-    img = cv2.imread(image_path)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def extract_keypoints_from_image(image):
+    img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = pose.process(img_rgb)
     if results.pose_landmarks:
         keypoints = []
@@ -24,14 +24,17 @@ def extract_keypoints_from_image(image_path):
 def lstm_Analysis_per1(image):
     check_keypoints = extract_keypoints_from_image(image)
 
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+    MODEL_PATH = os.path.join(BASE_PATH, "lstm_model")
+
     # LSTM 모델 불러오기 및 예측
-    model = load_model('lstm_model/best_1500.keras')
+    model = load_model(os.path.join(MODEL_PATH,'best_1500.keras'))
 
     if check_keypoints is not None:
         check_input = check_keypoints.reshape(1, 1, 99)
         pred = model.predict(check_input)
         pred_label = np.argmax(pred)
-        print('킥보드 탑승자' if pred_label == 1 else '보행자')
+        print('✅ 킥보드 탑승자' if pred_label == 1 else '✅ 보행자')
         return pred_label
     else:
         print("관절 데이터 추출 실패(사람이 인식되지 않음)")
